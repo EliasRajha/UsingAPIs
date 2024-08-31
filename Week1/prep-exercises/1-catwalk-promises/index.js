979deb191be70122e953d7964ad344201077d089
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-08-31 23:50:32 +0200

    week1 exercise

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -6,12 +6,27 @@ const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
+// Event to restart the loop 
+const repeatEvent = new Event("repeat");
+
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
     // Resolve this promise when the cat (`img`) has walked from `startPos` to
     // `stopPos`.
     // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
     // constants.
+    let currentPos = startPos;
+    img.style.left = currentPos + 'px';
+    const intervalId = setInterval(() => {
+      currentPos += STEP_SIZE_PX;
+      img.style.left = currentPos + 'px';
+      if (stopPos <= currentPos) {
+        // stop calling the interval
+        clearInterval(intervalId);
+        // Resolve this promise when the cat (`img`) has walked from `startPos` to `stopPos`.
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
@@ -21,6 +36,12 @@ function dance(img) {
     // and, after a timeout, reset the `img` back to the walking cat. Then
     // resolve the promise.
     // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+    let oldSource = img.src;
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      img.src = oldSource;
+      resolve();  
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -35,6 +56,24 @@ function catWalk() {
   // 2. Then dance for 5 secs.
   // 3. Then walk from `centerPos` to `stopPos`.
   // 4. Repeat the first three steps indefinitely.
+  const walkCenterPromise = walk(img, startPos, centerPos);
+  walkCenterPromise.then(() => {
+    console.log('cat is at ' + centerPos);
+    // cat dance for 5 seconds
+    const dancePromise = dance(img, centerPos, stopPos);
+    dancePromise.then(() => {
+      console.log('cat is dancing');
+      // cat continue walking to the end
+      const walkEndPromise = walk(img, centerPos, stopPos);
+      walkEndPromise.then(() => {
+        console.log('cat is at ' + stopPos);
+        // Now dispatch repeat event to restart the process
+        // code from https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events
+        window.dispatchEvent(repeatEvent);
+      });
+    });
+  });
 }
 
 window.addEventListener('load', catWalk);
+window.addEventListener('repeat', catWalk);
